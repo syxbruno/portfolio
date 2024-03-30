ee26aed7322b05a8c68cd862c20df121a8d9fecc
bruno oliveira <syxbrunoo@gmail.com>
2024-03-30 15:28:45 -0300

    atualizaçao

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -7,11 +7,12 @@ window.addEventListener('scroll', function(){
 
 
 
+
 let sunOrMoon = document.getElementById('sunOrMoon');
 
 sunOrMoon.onclick = function() {
     document.body.classList.toggle("tema-dark");
-    if(document.body.classList.contains("tema-dark")) {
+    if (document.body.classList.contains("tema-dark")) {
         sunOrMoon.src = "image/moon.png";
     } else {
         sunOrMoon.src = "image/sun.png"
@@ -20,7 +21,59 @@ sunOrMoon.onclick = function() {
 
 
 
+
+
+var links = document.querySelectorAll('.mylink');
+
+links.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        var href = this.getAttribute('href');
+        showConfirmationModal(href);
+    });
+});
+
+function showConfirmationModal(href) {
+    document.getElementById('modal').style.display = 'block';
+    
+    document.getElementById('confirmButton').addEventListener('click', function() {
+        window.open(href, '_blank');
+        hideConfirmationModal();
+    });
+
+    document.getElementById('cancelButton').addEventListener('click', function() {
+        hideConfirmationModal();
+    });
+}
+
+function hideConfirmationModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+
+
+
+
 function loading() {
     document.getElementById('loadingBody').style.display = "none";
     document.getElementById('beforeLoading').style.display = "block"
 }
+
+
+
+
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+    cursorOutline.animate({
+        left: `${posX}px` ,
+        top: `${posY}px`
+    }, {duration: 400, fill: "forwards"})
+}); 
